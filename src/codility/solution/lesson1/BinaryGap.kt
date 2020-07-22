10d5130bb91951303f687594d3648591e563982e
hiroshi-homma <homma-hiroshi@andfactpry.co.jp>
2020-07-22 23:56:56 +0900

    solutionLesson1Ver1 edit

--- src/codility/solution/lesson1/BinaryGap.kt
+++ src/codility/solution/lesson1/BinaryGap.kt
@@ -2,27 +2,6 @@ package codility.solution.lesson1
 
 object BinaryGap {
     fun solutionLesson1Ver1(N: Int): Int {
-        val binary = Integer.toBinaryString(N)
-        var count = 0
-        var tmpCount = 0
-        for (i in Integer.toBinaryString(N).indices) {
-            if (binary[i] == '0') {
-                if (i > 0 && binary[i - 1] == '1') {
-                    tmpCount++
-                } else {
-                    if (tmpCount > 0) tmpCount++
-                }
-            } else if (binary[i] == '1') {
-                if (tmpCount > 0 && tmpCount > count) {
-                    count = tmpCount
-                }
-                tmpCount = 0
-            }
-        }
-        return count
-    }
-
-    fun otherSolutionLesson1Ver1(N: Int): Int {
         val binaryNumber = Integer.toBinaryString(N)
         var maxLength = 0
         val gaps = binaryNumber.replace("0+$".toRegex(), "")
--- src/codility/solution/lesson1/binaryGap/BinayGapContentsDisplay.kt
+++ src/codility/solution/lesson1/binaryGap/BinayGapContentsDisplay.kt
@@ -9,9 +9,5 @@ object DemoTaskContentsDisplay {
         println(BinaryGap.solutionLesson1Ver1(1041))
         println(BinaryGap.solutionLesson1Ver1(15))
         println(BinaryGap.solutionLesson1Ver1(32).toString() + "\n------------------")
-        println("BinaryGapOther1")
-        println(BinaryGap.otherSolutionLesson1Ver1(1041))
-        println(BinaryGap.otherSolutionLesson1Ver1(15))
-        println(BinaryGap.otherSolutionLesson1Ver1(32).toString() + "\n------------------")
     }
 }
